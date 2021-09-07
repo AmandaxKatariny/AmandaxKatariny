0cba2255ec2dcd29d7e0339761d43dd430888659
Amanda Katariny <47179917+AmandaxKatariny@users.noreply.github.com>
2021-09-07 20:18:58 -0300

    Create NubankTestCandidateSelection.php

---
+++ NubankTestCandidateSelection.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Complete the 'formacaoDeTime' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY pontuacao
+ *  2. INTEGER tamanho_do_time
+ *  3. INTEGER k
+ */
+function somaPontuacao(array $timeSelecionado){
+    return array_sum($timeSelecionado); 
+}
+
+function selecionaMelhorPontuacao(array $candidatos, int $k){
+    $tamanhoDoArrayCandidatos = count($candidatos);
+    
+    $primeiroGrupo = array_slice($candidatos,0,$k, true);
+    $ultimoGrupo = array_slice($candidatos, -$k, $tamanhoDoArrayCandidatos-1, true);
+    
+    $keyMaiorPrimeiroGrupo = array_search(max($primeiroGrupo), $primeiroGrupo);
+    $keyMaiorUltimoGrupo = array_search(max($ultimoGrupo), $ultimoGrupo);
+
+    $keyMaior = max($primeiroGrupo) > max($ultimoGrupo) ? $keyMaiorPrimeiroGrupo : $keyMaiorUltimoGrupo;
+
+    return $keyMaior;
+}
+
+function checkRestricoes($pontuacao, $tamanho_do_time, $k)
+{   
+    $tamanhoArrayPontuacao = count($pontuacao);
+    
+    $restricoesPontuacao = min($pontuacao)>=1 && max($pontuacao)<= 109? false:true;
+    $restricoesTamanhoTime = $tamanho_do_time>=1 && $tamanho_do_time<=$tamanhoArrayPontuacao ? false:true;
+    $restricoesTamanhoSegmentosArray = $k>=1 && $k<=$tamanhoArrayPontuacao ? false:true;
+    
+    return $restricoesPontuacao && $restricoesTamanhoTime && $restricoesTamanhoSegmentosArray;
+}
+
+function formacaoDeTime($pontuacao, $tamanho_do_time, $k) {
+    $checkRestricao = checkRestricoes($pontuacao, $tamanho_do_time, $k);
+    
+    if(!$checkRestricao){            
+        $timeSelecionado = array();
+        $tamanhoTimeSelecionado = count($timeSelecionado);
+    
+        for($tamanhoTimeSelecionado=0 ; $tamanhoTimeSelecionado < $tamanho_do_time; $tamanhoTimeSelecionado++) {        
+            $keyMelhorPontuacao = selecionaMelhorPontuacao($pontuacao, $k);
+            $melhorPontuacao = $pontuacao[$keyMelhorPontuacao];
+            array_push($timeSelecionado, $melhorPontuacao);
+            
+            unset($pontuacao[$keyMelhorPontuacao]);
+        }
+        
+        if($tamanhoTimeSelecionado == $tamanho_do_time){
+            $somatorio = somaPontuacao($timeSelecionado);
+            return $somatorio;
+        }else{
+            return $pontuacao;
+        }
+    }
+    
+}
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
